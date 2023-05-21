d0576b9a369199e72200e099541489b31cf42781
Matthew Sells <matthew.sells@proton.me>
2023-05-21 16:42:30 -0500

    [sc-185] add query request

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react'
 import { ClerkProvider } from "@clerk/clerk-react";
 import { RouterProvider } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import { router } from '@/config/router'
+import { queryClient } from '@/lib/query';
 
 export const App: FC = () => {
 	return (
 		<ClerkProvider publishableKey={import.meta.env.VITE_APP_CLERK_PUBLISHABLE_KEY}>
-			<RouterProvider router={router} />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
 		</ClerkProvider>
 	)
 }
--- src/components/layout/menu.tsx
+++ src/components/layout/menu.tsx
@@ -2,16 +2,16 @@ import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import { UserButton } from '@clerk/clerk-react'
 
+
 export const MenuLayout: FC = () => {
 	return (
 		<div>
+			<nav>
+				<UserButton />
+			</nav>
 
-		<nav>
-			<UserButton />
-		</nav>
-
-		<main>
-			<Outlet />
+			<main>
+				<Outlet />
 			</main>
 		</div>
 	)
