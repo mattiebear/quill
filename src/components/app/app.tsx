a478e63aac3901ae73580ed1420e3473ad12a9fa
Matthew Sells <matthew.sells@proton.me>
2023-05-21 17:14:35 -0500

    [sc-185] add store provider

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,14 +4,17 @@ import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 import { router } from '@/config/router'
-import { queryClient } from '@/lib/query';
+import { queryClient } from '@/lib/query-client';
+import { StaticStoreProvider } from '../store/static-store-provider';
 
 export const App: FC = () => {
 	return (
 		<ClerkProvider publishableKey={import.meta.env.VITE_APP_CLERK_PUBLISHABLE_KEY}>
-			<QueryClientProvider client={queryClient}>
-				<RouterProvider router={router} />
-			</QueryClientProvider>
+			<StaticStoreProvider>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+				</QueryClientProvider>
+			</StaticStoreProvider>
 		</ClerkProvider>
 	)
 }
