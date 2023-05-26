7061b7a17aabb9092b9a27f8686b5a98c695cac7
Matt Sells <13752442+mattiebear@users.noreply.github.com>
2023-05-26 07:20:01 -0500

    [sc-223] add base custom theme (#5)

--- src/lib/theme/theme-provider.tsx
+++ src/lib/theme/theme-provider.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { FC, PropsWithChildren } from 'react';
 
+import { theme } from './theme';
+
 export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
-	return <ChakraProvider>{children}</ChakraProvider>;
+	return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 };
