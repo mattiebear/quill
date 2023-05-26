fa8557729afdbc729f43248793baf6d4559b97b4
Matthew Sells <matthew.sells@proton.me>
2023-05-26 08:28:42 -0500

    [sc-221] set up testing lib

---
+++ src/components/rail/index.ts
@@ -0,0 +1,7 @@
+export * from './rail';
+export * from './rail-bottom';
+export * from './rail-content';
+export * from './rail-group';
+export * from './rail-icon';
+export * from './rail-item';
+export * from './rail-top';
---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './tests/setup.js',
+	},
+});
