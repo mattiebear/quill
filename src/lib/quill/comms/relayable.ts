316708456eb18e5236651eebe826d15136a9c747
Matthew Sells <matthew.sells@proton.me>
2023-09-25 18:48:53 -0500

    [sc-427] remove relyable

--- src/lib/quill/comms/relayable.ts
+++
@@ -1,9 +0,0 @@
-import { unsubscribe } from 'diagnostics_channel';
-
-export class Relayable {
-	private _relaySubscriptions: VoidFunction[] = [];
-
-	destroy() {
-		this._relaySubscriptions.forEach((unsubscribe) => unsubscribe());
-	}
-}
