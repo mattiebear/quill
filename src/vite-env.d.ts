db33ca66ea550799794d6d2eb4ca00a3d9fa109b
Matt Sells <13752442+mattiebear@users.noreply.github.com>
2023-09-25 19:57:03 -0500

    [sc-458] add application class (#60)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
 	readonly VITE_APP_API_BASE_URL: string;
 	readonly VITE_APP_CLERK_PUBLISHABLE_KEY: string;
+	readonly VITE_APP_WEBSOCKET_URL: string;
 }
 
 interface ImportMeta {
