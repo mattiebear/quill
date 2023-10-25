4bc1b7197559b35a7ee645de0c2b906ce7fa5aa4
Matthew Sells <matthew.sells@proton.me>
2023-10-25 08:26:01 -0500

    [sc-532] add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,12 @@
 module.exports = {
 	env: { browser: true, es2020: true },
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-	],
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:storybook/recommended",
+        "plugin:storybook/recommended"
+    ],
 	parser: '@typescript-eslint/parser',
 	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
 	plugins: ['react-refresh'],
