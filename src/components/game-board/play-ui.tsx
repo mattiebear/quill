6e437d1e599049913354983224f942abadcb76af
Matthew Sells <matthew.sells@proton.me>
2023-11-06 08:29:30 -0600

    [sc-520] add game session rail menu

--- src/components/game-board/play-ui.tsx
+++ src/components/game-board/play-ui.tsx
@@ -1,34 +1,23 @@
-import {
-	AspectRatio,
-	Box,
-	Button,
-	Flex,
-	IconButton,
-	Image,
-	SimpleGrid,
-	Text,
-} from '@chakra-ui/react';
-import { FC, useRef } from 'react';
-import Draggable from 'react-draggable';
+import { Box, Button } from '@chakra-ui/react';
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
 import { Path } from '@/config/routes';
-import { useRelay } from '@/lib/messaging';
-import { useTokenset } from '@/lib/quill/hooks/use-tokenset';
-import { ChangeZoom } from '@/lib/quill/messages/rendering/change-zoom';
 
-import { ZoomInIcon, ZoomOutIcon } from '../icon';
-import { usePlaceTokenAction } from './hooks/use-place-token-action';
+import {
+	ArrowsPointingOutIcon,
+	Cog6ToothIcon,
+	CursorArrowRaysIcon,
+	UserGroupIcon,
+} from '../icon';
+import { RailMenu, RailMenuContent, RailMenuItem } from '../rail-menu';
 import { SelectMapModal } from './select-map-modal';
+import { TokenSelector } from './token-selector';
 
 export const PlayUI: FC = () => {
-	const nodeRef = useRef<HTMLDivElement>(null);
-	const tokenset = useTokenset();
 	const navigate = useNavigate();
 	const { t } = useTranslation();
-	const { send } = useRelay();
-	const { action, selectToken } = usePlaceTokenAction();
 
 	const handleClickDone = async () => {
 		navigate(Path.GameSessions);
@@ -38,67 +27,36 @@ export const PlayUI: FC = () => {
 		<>
 			<SelectMapModal />
 
-			<Box position="absolute">
-				{/* TODO: Create a menu component that encapsulates all of this */}
-				{/* TODO: Save position to local storage */}
-				<Draggable defaultPosition={{ x: 40, y: 40 }} nodeRef={nodeRef}>
-					<Flex
-						ref={nodeRef}
-						bg="background.float"
-						borderRadius="3xl"
-						cursor="grab"
-						direction="column"
-						minW="22rem"
-						p={4}
+			<Box position="absolute" p={2}>
+				<RailMenu>
+					<RailMenuItem
+						icon={<CursorArrowRaysIcon />}
+						label={t('editor.menuLabel.select')}
+					/>
+					<RailMenuItem
+						icon={<ArrowsPointingOutIcon />}
+						label={t('editor.menuLabel.move')}
+					/>
+
+					<RailMenuItem
+						icon={<UserGroupIcon />}
+						label={t('editor.menuLabel.tokens')}
 					>
-						<Text color="text.body" fontSize="xl">
-							Play
-						</Text>
-
-						<Flex columnGap={2} direction="row" mb={2}>
-							<IconButton
-								aria-label={t('editor.zoomOut')}
-								icon={<ZoomOutIcon />}
-								onClick={() => send(new ChangeZoom('out'))}
-							/>
-							<IconButton
-								aria-label={t('editor.zoomIn')}
-								icon={<ZoomInIcon />}
-								onClick={() => send(new ChangeZoom('in'))}
-							/>
-						</Flex>
-
-						<SimpleGrid columns={3} spacing={2} mb={2}>
-							{tokenset.all.map((token) => (
-								<Button
-									key={token.id}
-									h="auto"
-									p={2}
-									onClick={() => selectToken(token.id)}
-									{...(token.id === action.id && {
-										// TODO: Use semantic value
-										bg: 'green.500',
-										_hover: {
-											bg: 'green.500',
-										},
-									})}
-								>
-									<AspectRatio w="full" ratio={1}>
-										<Image objectPosition="bottom center" src={token.iconSrc} />
-									</AspectRatio>
-								</Button>
-							))}
-						</SimpleGrid>
-
-						<Button
-							colorScheme="green"
-							variant="outline"
-							onClick={handleClickDone}
-						>
-							{t('common.done')}
-						</Button>
-					</Flex>
-				</Draggable>
+						<RailMenuContent>
+							<TokenSelector />
+						</RailMenuContent>
+					</RailMenuItem>
+					<RailMenuItem
+						icon={<Cog6ToothIcon />}
+						label={t('editor.menuLabel.settings')}
+					>
+						<RailMenuContent>
+							<Button colorScheme="green" onClick={handleClickDone}>
+								{t('common.done')}
+							</Button>
+						</RailMenuContent>
+					</RailMenuItem>
+				</RailMenu>
 			</Box>
 		</>
 	);
