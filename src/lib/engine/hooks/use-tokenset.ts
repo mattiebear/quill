0f4f56a5e10f9eb61f533477f09f86586263eab8
Matthew Sells <matthew.sells@proton.me>
2023-12-07 12:51:42 -0600

    [sc-595] cleanup

--- src/lib/engine/hooks/use-tokenset.ts
+++ src/lib/engine/hooks/use-tokenset.ts
@@ -1,16 +1,12 @@
-// import { useMemo } from 'react';
-
 interface Token {
 	id: string;
 	image: string;
-	// type: TileType;
 }
 
 const manifest: Token[] = [
 	{
 		id: '1',
 		image: '',
-		// type: 'floor',
 	},
 ];
 
