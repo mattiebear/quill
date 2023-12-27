df4a2f82dd2bcfdcadb7bad9cd06d2fd1a72217f
Matthew Sells <matthew.sells@proton.me>
2023-12-27 17:48:41 -0600

    [sc-436] remove import

--- src/lib/engine/events/inbound/complete-story.ts
+++ src/lib/engine/events/inbound/complete-story.ts
@@ -1,8 +1,6 @@
 export class CompleteStory {
 	public static event = 'complete-story';
 
-	constructor(private context: any) {}
-
 	async run() {
 		console.log('complete story');
 		// FIXME: Navigate to the story index
